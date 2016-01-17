2691ad8488a76d9d8206cbd0f35eb250dcd0152d
Alberto Navarrete Martín <alnavart@gmail.com>
2016-01-17 12:47:39 +0100

    Enable zuul routing

--- src/main/groovy/com/lastminute/space/Application.groovy
+++ src/main/groovy/com/lastminute/space/Application.groovy
@@ -2,12 +2,14 @@ package com.lastminute.space
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class Application {
 
     public static void main(String[] args) {
