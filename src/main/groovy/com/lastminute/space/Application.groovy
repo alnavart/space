93693ab41fc39056707f06460acffa72707cca8c
Alberto Navarrete Martín <alnavart@gmail.com>
2016-01-20 23:17:29 +0100

    Enable hystrix dashboard

--- src/main/groovy/com/lastminute/space/Application.groovy
+++ src/main/groovy/com/lastminute/space/Application.groovy
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
