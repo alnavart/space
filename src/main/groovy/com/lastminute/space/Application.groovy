41de6b58314d371f9dbbc2d0e1b992bd9dea99ae
Alberto Navarrete Martín <alnavart@gmail.com>
2016-01-29 21:52:14 +0100

    Eureka server moved to an independent microservice

--- src/main/groovy/com/lastminute/space/Application.groovy
+++ src/main/groovy/com/lastminute/space/Application.groovy
@@ -3,20 +3,18 @@ package com.lastminute.space
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableHystrixDashboard
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled", "true");
-        System.setProperty("spring.devtools.restart.exclude", "static/js/stop.js,static/js/sockjs*");
         SpringApplication.run(Application.class, args)
     }
 
